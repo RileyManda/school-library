7370cccca51d288ea2adbad0dd66ddddfc01c195
RileyManda <rileymanda69@gmail.com>
2023-09-03 20:14:42 +0200

    Created person class with getters and setters

--- person.rb
+++ person.rb
@@ -1,1 +1,18 @@
-# Person class
+class Person
+    attr_reader :id
+    attr_accessor :name, :age
+
+    def initialize(id, name = 'Unknown', age = 0, parent_permission = true)
+        @id = id
+        @name = name
+        @age = age
+    end
+
+    def of_age?
+        @age >= 18
+    end
+
+    def can_use_services?
+        @age >= 18 || parent_permission
+    end
+end
