4f6f353de5d8621b475bb8674265fa470ab5bcb0
RileyManda <rileymanda69@gmail.com>
2023-09-04 10:50:21 +0200

    Linter and syntax fixes

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,7 +1,7 @@
 require_relative 'decorator'
- class TrimmerDecorator < Decorator
-    def correct_name
+class TrimmerDecorator < Decorator
+  def correct_name
     trimmed_name = @nameable.correct_name[0..9]
-    trimmed_name.length == 10 ? trimmed_name : trimmed_name + "..."
+    trimmed_name.length == 10 ? trimmed_name : trimmed_name.concat('...')
   end
 end
