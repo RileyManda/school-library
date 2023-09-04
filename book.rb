25c1b3fcb2c6d7d0f79b7da06f8ccf32383d0ddb
RileyManda <rileymanda69@gmail.com>
2023-09-04 16:06:56 +0200

    Added rental association to book

--- book.rb
+++ book.rb
@@ -1,9 +1,14 @@
+require_relative 'rental'
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author , :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
+  end
+
+  def add_rental(rental)
+    rentals << rental
   end
 end
