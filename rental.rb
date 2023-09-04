d3c076bfdcad99e97f5152aaca64919592c880e2
RileyManda <rileymanda69@gmail.com>
2023-09-04 15:17:58 +0200

    Linter error fixes

--- rental.rb
+++ rental.rb
@@ -1,7 +1,7 @@
 class Rental
-  attr_accessor :date, :book , :person
+  attr_accessor :date, :book, :person
 
-  def initialize(date,book, person)
+  def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
