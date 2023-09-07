5cc0ef9cea6b1b53038617c4acbf9b6df9a7a1b9
RileyManda <rileymanda69@gmail.com>
2023-09-07 10:51:41 +0200

    Ommitted test vars

---
+++ practice.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+def my_fancy_method
+  inside_method = 'We are now inside the method.'
+  puts inside_method
+  # pry_coming = 'We are about to see how pry works!'
+  # binding.pry
+  frozen = 'Pry froze the program before it got to this point!'
+  puts frozen
+end
+
+my_fancy_method
